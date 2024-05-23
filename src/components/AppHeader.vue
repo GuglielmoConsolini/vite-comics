3107bb595abfc476463cc147764037ac29352ad3
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-05-23 17:17:37 +0200

    vite-comics header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,47 @@
 <script>
-
+  export default {
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#home",
+                },
+                {
+                    text: "COMICS",
+                    url: "#products",
+                },
+                {
+                    text: "MOVIES",
+                    url: "#who",
+                },
+                {
+                    text: "TV",
+                    url: "#contacts",
+                },
+                {
+                    text: "GAMES",
+                    url: "#contacts",
+                },
+            ],
+        }
+    }
+}
 </script>
 
 <template>
-<h1>HEADER</h1>
+<nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/dc-logo.png" alt="Bootstrap" width="50" height="50">
+    </a>
+    <a v-for="link in links" :href="link.url">{{ link.text }}</a>
+  </div>
+</nav>
 </template>
 
-<style>
-
+<style scoped>
+  a{
+    text-decoration: none;
+  }
 </style>
